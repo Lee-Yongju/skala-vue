16d7ce27a1d0c3b333ed69de5e23468e9b896bdb
Lee-yongju <wkdrns717@gmail.com>
2026-08-24 17:42:06 +0900

    chore/day1 practice done

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+import VuePre from './components/practices/basic/VuePre.vue'
+import VueCloak from './components/practices/basic/VueCloak.vue'
+import VueOnce from './components/practices/basic/VueOnce.vue'
+import VueMemo from './components/practices/basic/VueMemo.vue'
+</script>
+
+<template>
+  <div style="padding: 20px">
+    <SampleOne />
+    <SampleTwo />
+    <hr />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <br />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <br />
+    <VueIf />
+    <VueShow />
+    <br />
+    <VueFor />
+    <br />
+    <VuePre />
+    <VueCloak />
+    <VueOnce />
+    <VueMemo />
+    <hr />
+  </div>
+</template>
+
+<style>
+@import '@/assets/practice.css';
+</style>
